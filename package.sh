1e1627ea3b4d7d2b855e6d7ce4d408dac2a3fdb0
Javier Cacheiro <jlopez@cesga.es>
2021-04-28 12:57:59 +0200

    Generate the package so it is extracted in a directory called notebooks

--- package.sh
+++ package.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-tar czvf pyspark_course.tar.gz ../pyspark_course/README.md ../pyspark_course/*.ipynb ../pyspark_course/exercises/ ../pyspark_course/solutions/ 
+TMPDIR=$(mktemp -d)
+mkdir $TMPDIR/notebooks
+cp -a ../pyspark_course/README.md ../pyspark_course/*.ipynb ../pyspark_course/exercises/ ../pyspark_course/solutions/ $TMPDIR/notebooks
+cd $TMPDIR
+tar czvf notebooks.tar.gz notebooks
+
+echo "You can find the packaged notebooks at: $TMPDIR/notebooks.tar.gz"
