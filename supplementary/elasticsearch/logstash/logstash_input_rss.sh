8dc763c977a3c1825dfcd6e46821e1cf358501e2
Javier Cacheiro <javier.cacheiro.lopez@cesga.es>
2022-10-14 10:50:39 +0200

    install required plugin

--- supplementary/elasticsearch/logstash/logstash_input_rss.sh
+++ supplementary/elasticsearch/logstash/logstash_input_rss.sh
@@ -2,13 +2,16 @@
 
 source ~/bash/opensearch
 
-docker run -it --rm --name logstash --net host opensearchproject/logstash-oss-with-opensearch-output-plugin:7.16.2 -e '
+docker run -it --rm --name logstash --net host opensearchproject/logstash-oss-with-opensearch-output-plugin:7.16.2 /bin/bash -c "
+logstash-plugin install logstash-input-rss && 
+echo \"Installed logstash-input-rss\" &&
+logstash -e '
 
 input {
   rss {
-    url => "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
+    url => \"https://rss.nytimes.com/services/xml/rss/nyt/World.xml\"
     interval => 3600
-    tags => ["rss", "news"]
+    tags => [\"rss\", \"news\"]
   }
 }
 
@@ -18,11 +21,12 @@ output {
    }
 
    opensearch {
-     hosts => ["https://'${OPENSEARCH_HOST}':'${OPENSEARCH_PORT}'"]
-     index => "opensearch-logstash-docker-%{+YYYY.MM.dd}"
-     user => "'${OPENSEARCH_USER}'"
-     password => "'${OPENSEARCH_PASSWD}'"
+     hosts => [\"https://'${OPENSEARCH_HOST}':'${OPENSEARCH_PORT}'\"]
+     index => \"logstash-rss-%{+YYYY.MM.dd}\"
+     user => \"'${OPENSEARCH_USER}'\"
+     password => \"'${OPENSEARCH_PASSWD}'\"
      ssl => true
      ssl_certificate_verification => false
    }
- }'
+}'
+"
