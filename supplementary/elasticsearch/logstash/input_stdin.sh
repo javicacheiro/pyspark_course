f93aaaf27edfd77c0c350f463b74d6f05bfea6b6
Javier Cacheiro <javier.cacheiro.lopez@cesga.es>
2022-10-14 10:02:29 +0200

    updates

--- supplementary/elasticsearch/logstash/input_stdin.sh
+++ supplementary/elasticsearch/logstash/input_stdin.sh
@@ -5,7 +5,7 @@ source ~/bash/opensearch
 docker run -it --rm --name logstash --net host opensearchproject/logstash-oss-with-opensearch-output-plugin:7.16.2 -e 'input { stdin { } } output {
    opensearch {
      hosts => ["https://'${OPENSEARCH_HOST}':'${OPENSEARCH_PORT}'"]
-     index => "opensearch-logstash-docker-%{+YYYY.MM.dd}"
+     index => "logstash-stdin-%{+YYYY.MM.dd}"
      user => "'${OPENSEARCH_USER}'"
      password => "'${OPENSEARCH_PASSWD}'"
      ssl => true
