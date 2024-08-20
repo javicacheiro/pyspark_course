0bfaa2328c77dc28277efb1c6d6c6f75ede115d8
Javier Cacheiro <javier.cacheiro.lopez@cesga.es>
2024-08-20 20:53:22 +0200

    do not truncate output

--- extended/exercises/Unit_8_input_source_rate.py
+++ extended/exercises/Unit_8_input_source_rate.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     # Sink
     query = df.writeStream \
         .format('console') \
+        .option('truncate', 'false') \
         .outputMode('update') \
         .start()
 
