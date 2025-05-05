4ab7e70c89fd5205f0736ed260da1bf13252fb4d
Javier Cacheiro <javier.cacheiro.lopez@cesga.es>
2025-05-05 17:44:19 +0200

    updated for 2025

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -av --exclude=".*" --exclude="sync.sh" --exclude="clean.sh" --dry-run ./ /opt/cesga/cursos/pyspark_2024/
+rsync -av --exclude=".*" --exclude="sync.sh" --exclude="clean.sh" --dry-run ./ /opt/cesga/cursos/pyspark_2025/
 
 echo "This was run only in dry-run mode!!"
 echo "If it does what you want run the rsync command manually"
